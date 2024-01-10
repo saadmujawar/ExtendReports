ae47682544209fdbce7f80bd10a0536b421ca070
Saad <saadmujawar1212@gmail.com>
2024-01-10 22:11:39 +0530

    ExtendReports

---
+++ src/main/java/saad/ExtendReports/App.java
@@ -0,0 +1,13 @@
+package saad.ExtendReports;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
